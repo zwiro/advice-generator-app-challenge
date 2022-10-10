05d48cb8ba7cf321f9457b656ec75ce25fc827a4
zwiro <josiu100@gmail.com>
2022-10-10 23:23:56 +0200

    js update

--- script.js
+++ script.js
@@ -3,25 +3,31 @@ const headingText = document.querySelector('h1');
 const adviceNumber = document.querySelector('span');
 const randomAdviceButton = document.querySelector('.dice');
 
+// const randomAdvice = async () => {
+//     const advice = await getAdvice();
+//     adviceText.textContent = advice[0];
+//     adviceNumber.textContent = advice[1];
+//     headingText.classList.add('animation');
+// };
+
 const getAdvice = async () => {
     try {
-        const res = await axios.get('https://api.adviceslip.com/advice');
+        const res = await axios.get("https://api.adviceslip.com/advice");
         return [res.data.slip.advice, res.data.slip.id];
-    }
-    catch (e) {
+    } catch (e) {
         return ['No advices available!', ''];
     }
-}
+};
 
-const randomAdvice = async () => {
+headingText.addEventListener('animationend', () => {
+    headingText.classList.remove('animation');
+});
+
+randomAdviceButton.addEventListener('click', async () => {
     const advice = await getAdvice();
     headingText.classList.add('animation');
     adviceText.textContent = advice[0];
     adviceNumber.textContent = advice[1];
-}
+});
 
-randomAdviceButton.addEventListener('click', randomAdvice);
 
-headingText.addEventListener('animationend', () => {
-    headingText.classList.remove('animation');
-})
