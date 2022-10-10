a0e5bd4a33b33f817480ec3abe9cf2fea0fb8c3f
zwiro <josiu100@gmail.com>
2022-10-10 23:37:56 +0200

    readme update

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Users should be able to:
 ### Links
 
 - Solution URL: [Solution](https://your-solution-url.com)
-- Live Site URL: [Live Site](https://your-live-site-url.com)
+- Live Site URL: [Live Site](https://zwiro.github.io/advice-generator-app-challenge/)
 
 ## My process
 
--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const headingText = document.querySelector('h1');
 const adviceNumber = document.querySelector('span');
 const randomAdviceButton = document.querySelector('.dice');
 
-// const randomAdvice = async () => {
-//     const advice = await getAdvice();
-//     adviceText.textContent = advice[0];
-//     adviceNumber.textContent = advice[1];
-//     headingText.classList.add('animation');
-// };
-
 const getAdvice = async () => {
     try {
         const res = await axios.get("https://api.adviceslip.com/advice");
